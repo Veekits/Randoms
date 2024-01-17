d49f80081f787376e7c68e370e3ccafbe29e73f6
Veekits <vanessamutheu101@gmail.com>
2024-01-17 17:34:58 +0300

    Create single box scrap.py

---
+++ Web Scraping/single box scrap.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+url ="https://beautyclick.co.ke/?utm_source=Google+Ads&utm_medium=CPC&utm_campaign=Google+Ads_+Competitor+Keywords+ad&utm_id=Competitor+Campaign_Anniversary&gclid=CjwKCAiAqY6tBhAtEiwAHeRopRdBb2VRqhAXZ1V5IokUjXaD8gkqUI4efMoPyljb9XRp5wyBOlRlqhoCg9sQAvD_BwE"
+r = requests.get(url)
+
+soup = BeautifulSoup(r.text, "html.parser")
+
+boxes = soup.find_all("div",{"class":"product-inner pr"})
+print(boxes)
