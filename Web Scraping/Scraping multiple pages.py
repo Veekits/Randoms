a70be74dd595db2a062e74b7d1fd9e6f71255a52
Vanessa Mukita <120710490+Veekits@users.noreply.github.com>
2024-01-23 07:28:51 +0300

    innit

--- Web Scraping/Scraping multiple pages.py
+++ Web Scraping/Scraping multiple pages.py
@@ -2,49 +2,34 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-url = "https://www.jumia.co.ke/catalog/?q=garnier"
-r = requests.get(url)
-print(r)
-
-soup = BeautifulSoup(r.text, "html.parser")
-
-np = soup.find("div", {"class":"pg-w -ptm -pbxl"}).find("a", {"aria-label":"Next Page"}).get("href")
-cnp = "https://www.jumia.co.ke/"+np
-print(cnp)
-
 product_names = []
 product_prices = []
 
-all_product_names = []
-all_product_prices = []
+url = "https://www.jumia.co.ke/catalog/?q=garnier"
+r = requests.get(url)
 
-def scrape_page(soup):
+soup = BeautifulSoup(r.text, "html.parser")
+
+for i in range(0,7):
+    np = soup.find("div", {"class":"pg-w -ptm -pbxl"}).find("a", {"aria-label":"Next Page"}).get("href")
+    cnp = "https://www.jumia.co.ke/"+np
+
+    url = cnp
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
 
     name = soup.find_all("h3",{"class":"name"})
 
     for i in name:
         product_name = i.text
         product_names.append(product_name)
-    print(product_names)
 
     price = soup.find_all("div",{"class":"prc"})
-
     for i in price:
         product_price = i.text
         product_prices.append(product_price)
-    print(product_prices)
-    return(product_names, product_prices)
 
-def jumia_catalogue(cnp):
-
-
-    while True:
-        all_product_names.extend(product_names)
-        all_product_prices.extend(product_prices)
-        if not cnp:
-            break
-    return(all_product_names, all_product_prices)
-df = pd.DataFrame({"Product Name":all_product_names, "Product Price":all_product_prices})
+df = pd.DataFrame({"Product Name":product_names, "Product Price":product_prices})
 print(df)
-
-
+ 
+df.to_csv("Jumia Products.csv")
